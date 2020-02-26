2202c36328dcca7a303a259ac1146e4aa9abe316
Miruna Betianu <miruna.lbetianu@gmail.com>
2020-02-27 00:47:05 +0200

    router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './App.css';
+import {Switch,Route} from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component'
+
+const HatsPage = (props) => {
+  console.log(props);
+  return( <div>
+    <h1>HATS PAGE</h1>
+  </div>);
+}
 
 function App() {
   return (
     <div>
-        <HomePage></HomePage>
+        <Switch>
+          <Route exact path='/' component={HomePage}></Route>
+          <Route exact path='/shop/hats' component={HatsPage}></Route>
+        </Switch>
+
     </div>
   );
 }

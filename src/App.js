e42375b62b054de8979042a6c036c42e06772638
Miruna Betianu <miruna.lbetianu@gmail.com>
2020-02-29 14:41:50 +0200

    null display name

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignOutPage from './pages/sign-in-and-up/sign-in-and-up.component'
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 class App extends React.Component {
     constructor()
@@ -21,9 +21,22 @@ class App extends React.Component {
     unsubscribeFromAuth = null;
 
     componentDidMount(){
-      this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-        this.setState({currentUser: user});
-        console.log(user);
+      this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+            if(userAuth){
+              const userRef = await createUserProfileDocument(userAuth);
+
+              userRef.onSnapshot(snapshot => {
+                  this.setState({
+                    currentUser:{
+                      id: snapshot.id,
+                      ...snapshot.data()
+                    }
+                  })
+                  console.log(this.state);
+              });
+            }else
+              this.setState({currentUser: userAuth});
+              
       });
     }
 
@@ -36,7 +49,7 @@ class App extends React.Component {
     {
       return (
         <div>
-            <Header></Header>
+            <Header currentUser = {this.state.currentUser}></Header>
             <Switch>
               <Route exact path='/' component={HomePage}></Route>
               <Route exact path='/shop' component={ShopPage}></Route>

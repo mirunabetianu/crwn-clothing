0a244fb0d7737302e17a0fb85ed7c490321d19ee
Miruna Betianu <miruna.lbetianu@gmail.com>
2020-03-01 18:56:56 +0200

    cart dropdown

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {Switch,Route} from 'react-router-dom';
+import {Switch,Route, Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 
 import HomePage from './pages/homepage/homepage.component';
@@ -44,8 +44,8 @@ class App extends React.Component {
             <Header></Header>
             <Switch>
               <Route exact path='/' component={HomePage}></Route>
-              <Route exact path='/shop' component={ShopPage}></Route>
-              <Route path='/signin' component={SignInAndSignOutPage}></Route>
+              <Route path='/shop' component={ShopPage}></Route>
+              <Route exact path='/signin' render= {()=> this.props.currentUser ? (<Redirect to='/'/>): (<SignInAndSignOutPage/>)}></Route>
             </Switch>
     
         </div>
@@ -53,11 +53,15 @@ class App extends React.Component {
     }
 }
 
+const mapStateToProps = ({user}) => ({
+    currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(App);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -8,7 +8,12 @@ import {ReactComponent as Logo} from '../../assets/crown.svg';
 
 import { auth } from '../../firebase/firebase.utils';
 
-const Header = ({currentUser}) => (
+import { connect } from 'react-redux';
+
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import {CartDropdown} from '../../components/cart-dropdown/cart-dropdown.component';
+
+const Header = ({currentUser,hidden}) => (
     <div className='header'>
         <Link className='logo-container' to="/" >
             <Logo className='logo'></Logo>
@@ -28,8 +33,19 @@ const Header = ({currentUser}) => (
                 :
                 <Link className='option' to="/signin">SIGN IN</Link>
             }
+            <CartIcon></CartIcon>
         </div>
+        
+        {
+            hidden ? null:
+            <CartDropdown></CartDropdown>
+        }
     </div>
 )
 
-export default Header;
+const mapStateToProps = ({user:{currentUser}, cart:{hidden}}) => ({
+    currentUser,
+    hidden
+});
+
+export default connect(mapStateToProps)(Header);
